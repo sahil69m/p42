e1e37d7b57b6e75ed3bd4954f3417d86df272ddb
sahil69m <111514657+sahil69m@users.noreply.github.com>
2022-12-04 18:05:04 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,35 @@
+var canvas, backgroundImage;
+
+var questions;
+
+var question, contestant, quiz;
+
+
+function setup(){
+  canvas = createCanvas(850,400);
+  database = firebase.database();
+  quiz = new Quiz();
+//call the start() method inside the quiz class
+this.message.html("Thank You,Your Answer Has Been Submited");
+this.message.position(350,350);
+
+this.message.html("Thank You,Your Answer Has Been Submited");
+this.message.position();
+
+this.message.html("Thank You,Your Answer Has Been Submited");
+this.message.position(350,350);
+
+this.message.html("Thank You,Your Answer Has Been Submited");
+this.message.position(350);
+
+
+
+
+
+}
+
+
+function draw(){
+  background("pink");
+
+}
